b61ac4d5725e60ae64f627debeee1da24cc8cde9
Srikanth Saripalli <sbayreddy@gmail.com>
2025-07-19 23:12:26 +0530

    Second Commit

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/data")
     public String getData() {
-        return "DATA PAGE";
+        return "Data PAGE";
     }
 
     @GetMapping("/message")
